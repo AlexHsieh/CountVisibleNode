f004ca86463194763669ae9fc145800f7be2b430
Alex Hsieh <Alex.Hsieh@dyson.com>
2018-10-08 17:10:06 +0800

    Initial Commit

---
+++ CountVisableTree/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  CountVisableTree
+//
+//  Created by Alex Hsieh on 8/10/18.
+//  Copyright © 2018 Alex Hsieh. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
